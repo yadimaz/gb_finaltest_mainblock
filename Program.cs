5947b68a4977209c21f48e71aa66093d6d59a851
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-04-08 11:38:04 +0300

    Add programm

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
 
+Console.Clear();
+string[] mas = { "Hello", "2", "world" };
+int len = 0;
+foreach (string item in mas)
+{
+    if (item.Length < 4) len++;
+}
+
+string[] finalmas = new string[len];
+for (int i = 0, j = 0; i < mas.Length; i++)
+{
+    if (mas[i].Length < 4)
+    {
+        finalmas[j] = mas[i];
+        j++;
+    }
+}
+Console.Write($"[{string.Join(", ", mas)}] > [{string.Join(", ", finalmas)}]");
