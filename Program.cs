7071527623ee323905e0c03a64ab1b016e0cd58f
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-04-09 10:35:10 +0300

    Comments added. Schema edits.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System.Globalization;
 
 Console.Clear();
+
 string[] mas = { "Hello", "2", "world" };
+
+// Считаем количество элементов
 int len = 0;
 foreach (string item in mas)
 {
     if (item.Length < 4) len++;
 }
-
+// Копируем элементы в finaltest массив
 string[] finalmas = new string[len];
 for (int i = 0, j = 0; i < mas.Length; i++)
 {
@@ -17,4 +20,5 @@ for (int i = 0, j = 0; i < mas.Length; i++)
         j++;
     }
 }
+// Вывод результата
 Console.Write($"[{string.Join(", ", mas)}] > [{string.Join(", ", finalmas)}]");
